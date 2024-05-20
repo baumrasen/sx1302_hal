c14cd6b57f7bbb56ef56d489bd2d9b31e0c1976e
baumrasen <baumrasen@gmail.com>
2024-05-20 20:13:05 +0200

    reset_lgw for raspberry bookworm

--- tools/reset_lgw.sh
+++ tools/reset_lgw.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # This script is intended to be used on SX1302 CoreCell platform, it performs
 # the following actions:
@@ -12,82 +12,27 @@
 # GPIO mapping has to be adapted with HW
 #
 
-SX1302_RESET_PIN=23     # SX1302 reset
-SX1302_POWER_EN_PIN=18  # SX1302 power enable
-SX1261_RESET_PIN=22     # SX1261 reset (LBT / Spectral Scan)
-AD5338R_RESET_PIN=13    # AD5338R reset (full-duplex CN490 reference design)
+GPIO_CHIP="gpiochip0"
+RESET_GPIO="23"
+POWER_EN_GPIO="18"
+POWER_EN_LOGIC="18"
 
-WAIT_GPIO() {
-    sleep 0.1
-}
+GPIOSET="gpioset -m time -u 100000 ${GPIO_CHIP}"
 
-init() {
-    # setup GPIOs
-    echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1261_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$AD5338R_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
+# Enable gateway
+if [[ ${POWER_EN_GPIO} -ne 0 ]]; then
+    echo "Concentrator enabled through ${GPIO_CHIP}:${POWER_EN_GPIO} (using libgpiod)"
+    ${GPIOSET} ${POWER_EN_GPIO}=${POWER_EN_LOGIC} 2>/dev/null
+fi
 
-    # set GPIOs as output
-    echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1261_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/direction; WAIT_GPIO
-}
-
-reset() {
-    echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
-    echo "SX1261 reset through GPIO$SX1302_RESET_PIN..."
-    echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
-    echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
-
-    # write output for SX1302 CoreCell power_enable and reset
-    echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
-
-    echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-    echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-
-    echo "0" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-    echo "1" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-
-    echo "0" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
-    echo "1" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
-}
-
-term() {
-    # cleanup all GPIOs
-    if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]
-    then
-        echo "$SX1302_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+# Reset gateway
+for GPIO in ${RESET_GPIO//,/ }; do
+    if [[ ${GPIO} -ne 0 ]]; then
+        echo "Concentrator reset through ${GPIO_CHIP}:${GPIO} (using libgpiod)"
+        ${GPIOSET} "${GPIO}"=0 2>/dev/null
+        ${GPIOSET} "${GPIO}"=1 2>/dev/null
+        ${GPIOSET} "${GPIO}"=0 2>/dev/null
     fi
-    if [ -d /sys/class/gpio/gpio$SX1261_RESET_PIN ]
-    then
-        echo "$SX1261_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]
-    then
-        echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$AD5338R_RESET_PIN ]
-    then
-        echo "$AD5338R_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-}
-
-case "$1" in
-    start)
-    term # just in case
-    init
-    reset
-    ;;
-    stop)
-    reset
-    term
-    ;;
-    *)
-    echo "Usage: $0 {start|stop}"
-    exit 1
-    ;;
-esac
+done
 
 exit 0
